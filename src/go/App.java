bb2595f27d7b4c9b400b8db1d0ae689ac4ac19bc
wzy <396898166@qq.com>
2017-04-01 10:08:41 +0800

    chuangkou

--- src/go/App.java
+++ src/go/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		AppFrame frame = new AppFrame();
 	}
 
 }
--- src/go/AppFrame.java
+++ src/go/AppFrame.java
@@ -1,5 +1,20 @@
 package go;
 
-public class AppFrame {
+import javax.swing.JFrame;
 
+public class AppFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AppFrame(){
+		setTitle("Îå×ÓÆå");
+		setSize(500,500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
